7a48def5d320011a0a220f802846bbdb34ae8254
kalaneprashant4 <140946537+kalaneprashant4@users.noreply.github.com>
2023-09-18 20:16:45 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -128,7 +128,7 @@ async def _(event):
      await event.delete()
 
 btnn =[
-    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/{Config.SUPPORT}"), Button.url("cʜᴀɴɴᴇʟ 🇮🇳", url=f"t.me/{Config.CHANNEL}")],
+    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/Patilscripter_bot"), Button.url("cʜᴀɴɴᴇʟ 🇮🇳", url=f"t.me/patilscripter")],
     [Button.inline("cʟᴏꜱᴇ 🗑️", data="cls")]]
 
 
